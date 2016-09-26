1c305f7853f62ee45d13c101330e3ba4c3917116
bryant <bryanttgm92778@gmail.com>
2016-09-26 16:30:49 +0800

    upload lesson3

---
+++ Code/lesson2/myFirstApp2/my first app2/Calculator2.h
@@ -0,0 +1,13 @@
+//
+//  Calculator2.h
+//  my first app2
+//
+//  Created by bryant on 22/9/2016.
+//  Copyright © 2016 cpttm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calculator2 : NSObject
+
+@end
---
+++ Code/lesson2/myFirstApp2/my first app2/Calculator2.m
@@ -0,0 +1,13 @@
+//
+//  Calculator2.m
+//  my first app2
+//
+//  Created by bryant on 22/9/2016.
+//  Copyright © 2016 cpttm. All rights reserved.
+//
+
+#import "Calculator2.h"
+
+@implementation Calculator2
+
+@end
